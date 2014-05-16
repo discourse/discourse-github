5fe78417a0c0376de522dfa2e3c90d1205316f5b
Sam <sam.saffron@gmail.com>
2014-05-16 11:55:24 +1000

    GitHub based badges!

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Discourse Github Linkback
+# Discourse Github
 
 This plugin will create a link from a Github pull request or commit
 back to a Discourse post where it is mentioned.
-
 
 ## Installation
 
@@ -10,12 +9,14 @@ Follow the [plugin installation guide](https://meta.discourse.org/t/install-a-pl
 
 ## After Installation
 
-1. You need to enable the plugin on Settings -> Plugins.
+1.  You need to enable the plugin on Settings -> Plugins.
 
-2. Generate an [access token](https://github.com/settings/tokens) on Github.
-Be sure to give it <em>only</em> the `public_repo` scope. Paste that token into the
-`github linkback access token` setting.
+2.  Generate an [access token](https://github.com/settings/tokens) on Github.
+    Be sure to give it <em>only</em> the `public_repo` scope. Paste that token into the
+    `github linkback access token` setting.
 
-3. Finally, add the projects you wish to post to in the `github linkback projects` site setting in the formats: 
-    - `username/repository` for specific repositories 
+3.  Finally, add the projects you wish to post to in the `github linkback projects` site setting in the formats:
+    - `username/repository` for specific repositories
     - `username/*` for all repositories of a certain user
+
+Assign badges to your users based on GitHub contributions!
--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
-# name: discourse-github-linkback
-# about: Links Github content back to a Discourse discussion
+# name: discourse-github
+# about: Links Github content back to a Discourse discussion, Assign users badges based on GitHub contributions
 # version: 0.1
-# authors: Robin Ward
-# url: https://github.com/discourse/discourse-github-linkback
+# authors: Robin Ward, Sam Saffron
+# url: https://github.com/discourse/discourse-github
 
 enabled_site_setting :github_linkback_enabled
 
@@ -19,3 +19,83 @@ after_initialize do
   end
 end
 
+
+module ::GithubBadges
+  def self.badge_grant!
+
+    return unless SiteSetting.github_badges_repo.present?
+
+    # ensure badges exist
+    unless bronze = Badge.find_by(name: 'Contributor')
+      bronze = Badge.create!(name: 'Contributor',
+                             description: 'contributed an accepted pull request',
+                             badge_type_id: 3)
+    end
+
+    unless silver = Badge.find_by(name: 'Great contributor')
+      silver = Badge.create!(name: 'Great contributor',
+                             description: 'contributed 25 accepted pull request',
+                             badge_type_id: 2)
+    end
+
+    unless gold = Badge.find_by(name: 'Gmazing contributor')
+      gold = Badge.create!(name: 'Amazing contributor',
+                             description: 'contributed 250 accepted pull request',
+                             badge_type_id: 1)
+    end
+
+    emails = []
+
+    path = '/tmp/github_badges'
+
+    if !Dir.exists?(path)
+      Rails.logger.info `cd /tmp && git clone #{SiteSetting.github_badges_repo} github_badges`
+    else
+      Rails.logger.info `cd #{path} && git pull`
+    end
+
+    `cd #{path} && git log --merges --pretty=format:%p --grep='Merge pull request'`.each_line do |m|
+      emails << (`cd #{path} && git log -1 --format=%ce #{m.split(' ')[1].strip}`.strip)
+    end
+
+    email_commits = emails.group_by{|e| e}.map{|k, l|[k,l.count]}
+
+    Rails.logger.info "#{email_commits.length} commits found!"
+
+    email_commits.each do |email, commits|
+      user = User.find_by(email: email)
+
+      if user
+        if commits < 25
+          BadgeGranter.grant(bronze, user)
+        elsif commits < 250
+          BadgeGranter.grant(silver, user)
+          if user.title.blank?
+            user.title = silver.name
+            user.save
+          end
+        else
+          BadgeGranter.grant(gold, user)
+          if user.title.blank?
+            user.title = gold.name
+            user.save
+          end
+        end
+      end
+
+    end
+
+  end
+end
+
+after_initialize do
+  module ::GithubBadges
+    class UpdateJob < ::Jobs::Scheduled
+      every 1.day
+
+      def execute(args)
+        GithubBadges.badge_grant!
+      end
+    end
+  end
+end
