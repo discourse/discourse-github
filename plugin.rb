af2c8778708f0a63443fc71a3a12bcf3bf96e7fe
Régis Hanol <regis@hanol.fr>
2016-03-25 12:01:47 +0100

    FIX: grant lower levels badges

--- plugin.rb
+++ plugin.rb
@@ -19,61 +19,66 @@ after_initialize do
   end
 end
 
-module ::GithubBadges
-  def self.badge_grant!
 
-    return unless SiteSetting.github_badges_repo.present?
+module ::GithubBadges
+
+  BADGE_NAME_BRONZE ||= 'Contributor'.freeze
+  BADGE_NAME_SILVER ||= 'Great contributor'.freeze
+  BADGE_NAME_GOLD   ||= 'Amazing contributor'.freeze
+
+  def self.badge_grant!
+    return if SiteSetting.github_badges_repo.blank?
 
     # ensure badges exist
-    unless bronze = Badge.find_by(name: 'Contributor')
-      bronze = Badge.create!(name: 'Contributor',
+    unless bronze = Badge.find_by(name: BADGE_NAME_BRONZE)
+      bronze = Badge.create!(name: BADGE_NAME_BRONZE,
                              description: 'contributed an accepted pull request',
                              badge_type_id: 3)
     end
 
-    unless silver = Badge.find_by(name: 'Great contributor')
-      silver = Badge.create!(name: 'Great contributor',
-                             description: 'contributed 25 accepted pull request',
+    unless silver = Badge.find_by(name: BADGE_NAME_SILVER)
+      silver = Badge.create!(name: BADGE_NAME_SILVER,
+                             description: 'contributed 25 accepted pull requests',
                              badge_type_id: 2)
     end
 
-    unless gold = Badge.find_by(name: 'Amazing contributor')
-      gold = Badge.create!(name: 'Amazing contributor',
-                             description: 'contributed 250 accepted pull request',
-                             badge_type_id: 1)
+    unless gold = Badge.find_by(name: BADGE_NAME_GOLD)
+      gold = Badge.create!(name: BADGE_NAME_GOLD,
+                           description: 'contributed 250 accepted pull requests',
+                           badge_type_id: 1)
     end
 
     emails = []
-
     path = '/tmp/github_badges'
 
-    if !Dir.exists?(path)
-      Rails.logger.info `cd /tmp && git clone #{SiteSetting.github_badges_repo} github_badges`
-    else
+    if Dir.exists?(path)
       Rails.logger.info `cd #{path} && git pull`
+    else
+      Rails.logger.info `cd /tmp && git clone #{SiteSetting.github_badges_repo} github_badges`
     end
 
     `cd #{path} && git log --merges --pretty=format:%p --grep='Merge pull request'`.each_line do |m|
       emails << (`cd #{path} && git log -1 --format=%ce #{m.split(' ')[1].strip}`.strip)
     end
 
-    email_commits = emails.group_by{|e| e}.map{|k, l|[k,l.count]}
+    email_commits = emails.group_by { |e| e }.map { |k, l| [k, l.count] }
 
     Rails.logger.info "#{email_commits.length} commits found!"
 
     email_commits.each do |email, commits|
-      user = User.find_by(email: email)
+      if user = User.find_by(email: email)
 
-      if user
-        if commits < 25
-          BadgeGranter.grant(bronze, user)
-        elsif commits < 250
+        BadgeGranter.grant(bronze, user)
+
+        if commits >= 25
           BadgeGranter.grant(silver, user)
           if user.title.blank?
             user.title = silver.name
             user.save
           end
-        else
+        end
+
+        if commits >= 250
           BadgeGranter.grant(gold, user)
           if user.title.blank?
             user.title = gold.name
@@ -81,9 +86,7 @@ module ::GithubBadges
           end
         end
       end
-
     end
-
   end
 end
 
