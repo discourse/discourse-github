95d2243aa278c07d261d7a934073cf3342296f14
Maja Komel <maja.komel@gmail.com>
2019-01-27 16:23:59 +0100

    Fix github link replacements for posts with multiple urls

--- app/jobs/regular/replace_github_non_permalinks.rb
+++ app/jobs/regular/replace_github_non_permalinks.rb
@@ -24,12 +24,13 @@ module Jobs
       matches = post.raw.scan(regex)
       matches.each do |user, repo, sha1, file, from_to|
         begin
-          url = "https://api.github.com/repos/#{user}/#{repo}/commits/#{sha1}"
-          json = api_request(url)
+          api_url = "https://api.github.com/repos/#{user}/#{repo}/commits/#{sha1}"
+          json = api_request(api_url)
           if json && (json["sha"] != sha1)
             new_sha = json["sha"]
-            url = "github.com/#{user}/#{repo}/blob/#{new_sha}/#{file}#{from_to}"
-            raw.sub!(regex, url)
+            old_url = "github.com/#{user}/#{repo}/blob/#{sha1}/#{file}#{from_to}"
+            new_url = "github.com/#{user}/#{repo}/blob/#{new_sha}/#{file}#{from_to}"
+            raw.sub!(old_url, new_url)
           end
         rescue => e
           log(:error, "Failed to replace Github link with permalink in post #{post_id}\n" + e.message + "\n" + e.backtrace.join("\n"))
--- spec/jobs/replace_github_non_permalinks_spec.rb
+++ spec/jobs/replace_github_non_permalinks_spec.rb
@@ -4,8 +4,11 @@ require 'jobs/regular/pull_hotlinked_images'
 describe Jobs::ReplaceGithubNonPermalinks do
   let(:github_url) { "https://github.com/test/onebox/blob/master/lib/onebox/engine/github_blob_onebox.rb" }
   let(:github_permanent_url) { "https://github.com/test/onebox/blob/815ea9c0a8ffebe7bd7fcd34c10ff28c7a6b6974/lib/onebox/engine/github_blob_onebox.rb" }
+  let(:github_url2) { "https://github.com/test/discourse/blob/master/app/models/tag.rb#L1-L3" }
+  let(:github_permanent_url2) { "https://github.com/test/discourse/blob/7e4edcfae8a3c0e664b836ee7c5f28b47853a2f8/app/models/tag.rb#L1-L3" }
   let(:broken_github_url) { "https://github.com/test/oneblob/blob/master/lib/onebox/engine/nonexistent.rb" }
   let(:github_response_body) { { sha: '815ea9c0a8ffebe7bd7fcd34c10ff28c7a6b6974', commit: {} } }
+  let(:github_response_body2) { { sha: '7e4edcfae8a3c0e664b836ee7c5f28b47853a2f8', commit: {} } }
 
   before do
     stub_request(:get, "https://api.github.com/repos/test/onebox/commits/master")
@@ -13,6 +16,8 @@ describe Jobs::ReplaceGithubNonPermalinks do
     stub_request(:get, "https://api.github.com/repos/test/onebox/commits/815ea9c0a8ffebe7bd7fcd34c10ff28c7a6b6974")
       .to_return(status: 200, body: github_response_body.to_json, headers: {})
     stub_request(:get, "https://api.github.com/repos/test/oneblob/commits/master").to_return(status: 404)
+    stub_request(:get, "https://api.github.com/repos/test/discourse/commits/master")
+      .to_return(status: 200, body: github_response_body2.to_json, headers: {})
   end
 
   describe '#execute' do
@@ -44,6 +49,15 @@ describe Jobs::ReplaceGithubNonPermalinks do
       post.reload
 
       expect(post.raw).to eq(broken_github_url)
+    end
+
+    it "works with multiple github urls in the post" do
+      post = Fabricate(:post, raw: "#{github_url} #{github_url2} htts://github.com")
+      Jobs::ReplaceGithubNonPermalinks.new.execute(post_id: post.id)
+      post.reload
+
+      updated_post = "#{github_permanent_url} #{github_permanent_url2} htts://github.com"
+      expect(post.raw).to eq(updated_post)
     end
   end
 end
