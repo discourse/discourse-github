1bc4e1b39270e2715b308236fdd99c667367e793
Osama Sayegh <asooomaasoooma90@gmail.com>
2019-06-13 13:33:21 +0300

    FEATURE: Committer badges (#5)

--- app/lib/github_badges.rb
+++ app/lib/github_badges.rb
@@ -2,9 +2,48 @@
 
 module ::GithubBadges
 
+  def self.TMP_DIR
+    @@tmp_dir ||= "/tmp/github_badges"
+  end
+
+  # used in tests
+  def self.TMP_DIR=(path)
+    @@tmp_dir = path
+  end
+
+  class Granter
+    def initialize(emails)
+      @emails = emails
+      @badges = []
+    end
+
+    def add_badge(badge, as_title:, &blk)
+      @badges << [badge, as_title, blk]
+    end
+
+    def grant!
+      email_commits = @emails.group_by { |e| e }.map { |k, l| [k, l.count] }.to_h
+      User.with_email(email_commits.keys).each do |user|
+        commits_count = email_commits[user.email]
+        @badges.each do |badge, as_title, threshold|
+          if threshold.call(commits_count) && badge.enabled? && SiteSetting.enable_badges
+            BadgeGranter.grant(badge, user)
+            if user.title.blank? && as_title
+              user.update_attributes!(title: badge.name)
+            end
+          end
+        end
+      end
+    end
+  end
+
   BADGE_NAME_BRONZE ||= 'Contributor'.freeze
   BADGE_NAME_SILVER ||= 'Great contributor'.freeze
   BADGE_NAME_GOLD   ||= 'Amazing contributor'.freeze
+
+  COMMITER_BADGE_NAME_BRONZE ||= 'Committer'.freeze
+  COMMITER_BADGE_NAME_SILVER ||= 'Frequent committer'.freeze
+  COMMITER_BADGE_NAME_GOLD   ||= 'Amazing committer'.freeze
 
   def self.badge_grant!
     return if SiteSetting.github_badges_repo.blank?
@@ -29,40 +68,64 @@ module ::GithubBadges
     end
 
     emails = []
-    path = '/tmp/github_badges'
-
-    if Dir.exists?(path)
-      Rails.logger.info `cd #{path} && git pull`
-    else
-      Rails.logger.info `cd /tmp && git clone #{SiteSetting.github_badges_repo} github_badges`
-    end
-
-    `cd #{path} && git log --merges --pretty=format:%p --grep='Merge pull request'`.each_line do |m|
-      emails << (`cd #{path} && git log -1 --format=%ce #{m.split(' ')[1].strip}`.strip)
-    end
-
-    email_commits = emails.group_by { |e| e }.map { |k, l| [k, l.count] }.to_h
-
-    Rails.logger.info "#{email_commits.length} commits found!"
-
-    User.with_email(email_commits.keys).each do |user|
-      commits = email_commits[user.email]
-
-      BadgeGranter.grant(bronze, user)
-
-      if commits >= SiteSetting.github_silver_badge_min_commits
-        BadgeGranter.grant(silver, user)
-        if user.title.blank?
-          user.update_attributes!(title: silver.name)
-        end
-      end
-
-      if commits >= SiteSetting.github_gold_badge_min_commits
-        BadgeGranter.grant(gold, user)
-        if user.title.blank?
-          user.update_attributes!(title: gold.name)
-        end
+    if !Rails.env.test?
+      if Dir.exists?(self.TMP_DIR)
+        Rails.logger.info `cd #{self.TMP_DIR} && git pull`
+      else
+        Rails.logger.info `git clone #{SiteSetting.github_badges_repo} #{self.TMP_DIR}`
       end
     end
+
+    Dir.chdir(self.TMP_DIR) do
+      `git log --merges --pretty=format:%p --grep='Merge pull request'`.each_line do |m|
+        emails << (`git log -1 --format=%ce #{m.split(' ')[1].strip}`.strip)
+      end
+    end
+
+    granter = GithubBadges::Granter.new(emails)
+    granter.add_badge(bronze, as_title: false) { |commits| commits >= 1 }
+    granter.add_badge(silver, as_title: true) { |commits| commits >= SiteSetting.github_silver_badge_min_commits }
+    granter.add_badge(gold, as_title: true) { |commits| commits >= SiteSetting.github_gold_badge_min_commits }
+    granter.grant!
+
+    committer_badge_grant!
+  end
+
+  def self.committer_badge_grant!
+    unless bronze = Badge.find_by(name: COMMITER_BADGE_NAME_BRONZE)
+      bronze = Badge.create!(name: COMMITER_BADGE_NAME_BRONZE,
+                             description: 'created a commit',
+                             enabled: false,
+                             badge_type_id: 3)
+    end
+
+    unless silver = Badge.find_by(name: COMMITER_BADGE_NAME_SILVER)
+      silver = Badge.create!(name: COMMITER_BADGE_NAME_SILVER,
+                             description: 'created 25 commits',
+                             enabled: false,
+                             badge_type_id: 2)
+    end
+
+    unless gold = Badge.find_by(name: COMMITER_BADGE_NAME_GOLD)
+      gold = Badge.create!(name: COMMITER_BADGE_NAME_GOLD,
+                           description: 'created 1000 commits',
+                           enabled: false,
+                           badge_type_id: 1)
+    end
+
+    emails = []
+    Dir.chdir(self.TMP_DIR) do
+      `git log --no-merges --pretty=format:%H`.each_line do |h|
+        emails << (`git log -1 --format=%ae #{h}`.strip)
+        message = `git log -1 --format=%b #{h}`.strip
+        message.scan(/co-authored-by:.+<(.+@.+)>/i).flatten.each { |e| emails << e }
+      end
+    end
+
+    granter = GithubBadges::Granter.new(emails)
+    granter.add_badge(bronze, as_title: false) { |commits| commits >= 1 }
+    granter.add_badge(silver, as_title: true) { |commits| commits >= 25 }
+    granter.add_badge(gold, as_title: true) { |commits| commits >= 1000 }
+    granter.grant!
   end
 end
