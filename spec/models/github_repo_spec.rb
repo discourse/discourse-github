efdf57cc5f8cf9e9a22e182a635ca7999d5f833a
Robin Ward <robin.ward@gmail.com>
2019-07-03 15:37:49 -0400

    Use core codebase style for changing site settings in specs

--- spec/models/github_repo_spec.rb
+++ spec/models/github_repo_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 describe DiscourseGithubPlugin::GithubRepo do
 
   it "strips .git from url" do
-    SiteSetting.set("github_badges_repos", "https://github.com/discourse/discourse.git")
+    SiteSetting.github_badges_repos = "https://github.com/discourse/discourse.git"
     repo = DiscourseGithubPlugin::GithubRepo.repos.first
     expect(repo.name).to eq ("discourse/discourse")
   end
 
   it "strips trailing slash from url" do
-    SiteSetting.set("github_badges_repos", "https://github.com/discourse/discourse/")
+    SiteSetting.github_badges_repos = "https://github.com/discourse/discourse/"
     repo = DiscourseGithubPlugin::GithubRepo.repos.first
     expect(repo.name).to eq ("discourse/discourse")
   end
