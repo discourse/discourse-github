2993eb186af83767b420b791b591dab15810e067
OsamaSayegh <asooomaasoooma90@gmail.com>
2020-12-10 22:04:52 +0300

    add comment to migration

--- db/migrate/20201210032852_discourse_github_rebuild_git_history.rb
+++ db/migrate/20201210032852_discourse_github_rebuild_git_history.rb
@@ -2,6 +2,10 @@
 
 class DiscourseGithubRebuildGitHistory < ActiveRecord::Migration[6.0]
   def up
+    # the table will be repopulated the next time the `UpdateJob` runs.
+    # This will not have any noticeable effects on the site because
+    # this table is merely used to grant the committer and contributor
+    # badges. Deleting commits will not cause users to lose badges.
     execute "DELETE FROM github_commits"
   end
 
