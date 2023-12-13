081aea3e1992a64d3b9306cb235bc7f4f80ee505
Jarek Radosz <jradosz@gmail.com>
2023-12-13 12:50:53 +0100

    DEV: Update linting

--- spec/models/github_repo_spec.rb
+++ spec/models/github_repo_spec.rb
@@ -6,22 +6,22 @@ describe DiscourseGithubPlugin::GithubRepo do
   it "strips .git from url" do
     SiteSetting.github_badges_repos = "https://github.com/discourse/discourse.git"
     repo = DiscourseGithubPlugin::GithubRepo.repos.first
-    expect(repo.name).to eq ("discourse/discourse")
+    expect(repo.name).to eq("discourse/discourse")
   end
 
   it "strips trailing slash from url" do
     SiteSetting.github_badges_repos = "https://github.com/discourse/discourse/"
     repo = DiscourseGithubPlugin::GithubRepo.repos.first
-    expect(repo.name).to eq ("discourse/discourse")
+    expect(repo.name).to eq("discourse/discourse")
   end
 
   it "doesn't raise an error when the site setting follows the user/repo format" do
     SiteSetting.github_badges_repos = "discourse/discourse-github"
     repo = DiscourseGithubPlugin::GithubRepo.repos.first
-    expect(repo.name).to eq ("discourse/discourse-github")
+    expect(repo.name).to eq("discourse/discourse-github")
 
     SiteSetting.github_badges_repos = "discourse/somerepo_with-numbers7"
     repo = DiscourseGithubPlugin::GithubRepo.repos.first
-    expect(repo.name).to eq ("discourse/somerepo_with-numbers7")
+    expect(repo.name).to eq("discourse/somerepo_with-numbers7")
   end
 end
